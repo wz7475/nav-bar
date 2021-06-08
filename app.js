b2476f0bff95d8ac0773942d3135fef981715dc4
Wojciech Zarzecki <wojtek.zarzecki2002@gmail.com>
2021-06-08 11:50:09 +0200

    finish options' animation in moble navabr

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
-
+    const navLinks = document.querySelectorAll(".nav-links li");
     burger.addEventListener("click", () =>{
         nav.classList.toggle("nav-active");
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ""
+            } else {
+                link.style.animation = `navLinkFade 0.5s forwards ${index / 7 + 0.5}s`;
+            }
+        });
+
     });
 }
 
