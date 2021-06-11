73b34d8363863c7dff73fbf7a1c39c369dc5aff2
Wojciech Zarzecki <wojtek.zarzecki2002@gmail.com>
2021-06-11 09:21:52 +0200

    working on grid gallery

---
+++ gallery.js
@@ -0,0 +1,19 @@
+const generate_gallery = () =>{
+    main = document.querySelector("main");
+    for (let i=0; i < 4; i++){
+        col = document.createElement("div");
+        col.id = i
+        // main.appendChild(col);
+        for (let i=0; i < 16; i++){
+            img = document.createElement("img");
+            // img.src = `https://source.unsplash.com/200x300?random=${i+1}/`;
+            img.src = `https://picsum.photos/200/300?random=${i+1}`;
+            img.alt = "picture";
+            img.className = "img" + i;
+            col.appendChild(img);
+        }
+        main.appendChild(col);
+    }
+}
+
+generate_gallery();
