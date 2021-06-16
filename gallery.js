7f78e182592f2d2b3ebf86ba4465428dd353708d
Wojciech Zarzecki <wojtek.zarzecki2002@gmail.com>
2021-06-16 08:48:05 +0200

    working on gallery, not scrollable on mobile, issue is overflow hidden below 768px

--- gallery.js
+++ gallery.js
@@ -1,5 +1,6 @@
 const generate_gallery = () =>{
     main = document.querySelector("main");
+    let counter = 1;
     for (let i=0; i < 4; i++){
         col = document.createElement("div");
         col.id = i
@@ -7,7 +8,8 @@ const generate_gallery = () =>{
         for (let i=0; i < 16; i++){
             img = document.createElement("img");
             // img.src = `https://source.unsplash.com/200x300?random=${i+1}/`;
-            img.src = `https://picsum.photos/200/300?random=${i+1}`;
+            img.src = `https://picsum.photos/200/300?random=${counter}`;
+            counter++;
             img.alt = "picture";
             img.className = "img" + i;
             col.appendChild(img);
